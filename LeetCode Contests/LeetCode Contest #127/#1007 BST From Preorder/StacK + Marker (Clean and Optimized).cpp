ae2241eb0909b9209d05a174fc11c7fca0eb6d89
Anonymous <48237538+Just-A-Visitor@users.noreply.github.com>
2019-03-16 22:58:12 +0530

    Create StacK + Marker (Clean and Optimized).cpp

---
+++ LeetCode Contests/LeetCode Contest #127/#1007 BST From Preorder/StacK + Marker (Clean and Optimized).cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder);
+};
+
+/* Constructs the binary search tree whose preorder is given */
+TreeNode* Solution :: bstFromPreorder(vector<int>& preorder)
+{
+    // Base case ---- The tree must exist
+    if(preorder.size()==0) return nullptr;
+    
+    // Create the stack to hold nodes, not values
+    stack<TreeNode*> myStack;
+    TreeNode* root;
+    
+    /* An amazing trick to handle empty cases. The Marker technique */
+    /* The stack can never be empty now */
+    myStack.push(new TreeNode(INT_MAX));
+    
+    // Traverse each element of the array and add it to the correct place
+    for(auto ele : preorder)
+    {
+        // Create the new node to be attached
+        TreeNode* current = new TreeNode(ele);
+        
+        // If the stack contains the marker, update the root
+        if(myStack.top()->val == INT_MAX) root = current;
+        else
+        {
+            // Pointer to record the last element popped
+            TreeNode* lastPopped = nullptr;
+            
+            // As long as the current value is greater, keep popping
+            while(ele > myStack.top()->val)
+                lastPopped = myStack.top(), myStack.pop();
+            
+            // If nothing was popped, it means it was smaller, hence left child
+            if(!lastPopped)
+                myStack.top()->left = current;
+            else 
+                lastPopped->right = current;
+        }
+        
+        // Remember to push the new node . This is necessary to keep going
+        myStack.push(current);
+    }
+    
+    return root;
+}
+
+/* An important point to note that we are modifying the value without popping. How? */
+/* This is possible because of pointers. Making a copy can still modify elements. */
