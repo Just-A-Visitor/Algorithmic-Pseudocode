e237996e28033c098a901a252e22fb92bb9a6309
Anonymous <48237538+Just-A-Visitor@users.noreply.github.com>
2019-03-16 23:03:59 +0530

    Update Stack + Marker (Clean and Optimized).cpp

--- LeetCode Contests/LeetCode Contest #127/#1007 BST From Preorder/Stack + Marker (Clean and Optimized).cpp
+++ LeetCode Contests/LeetCode Contest #127/#1007 BST From Preorder/Stack + Marker (Clean and Optimized).cpp
@@ -12,35 +12,34 @@ TreeNode* Solution :: bstFromPreorder(vector<int>& preorder)
     
     // Create the stack to hold nodes, not values
     stack<TreeNode*> myStack;
-    TreeNode* root;
+    TreeNode* root = new TreeNode(preorder.front());
     
     /* An amazing trick to handle empty cases. The Marker technique */
     /* The stack can never be empty now */
     myStack.push(new TreeNode(INT_MAX));
+    myStack.push(root);
     
     // Traverse each element of the array and add it to the correct place
     for(auto ele : preorder)
     {
+        // Skip the first element, i.e, root, as it has been pushed
+        if(ele == preorder.front()) continue;
+        
         // Create the new node to be attached
         TreeNode* current = new TreeNode(ele);
         
-        // If the stack contains the marker, update the root
-        if(myStack.top()->val == INT_MAX) root = current;
-        else
-        {
-            // Pointer to record the last element popped
-            TreeNode* lastPopped = nullptr;
+        // Pointer to record the last element popped
+        TreeNode* lastPopped = nullptr;
             
-            // As long as the current value is greater, keep popping
-            while(ele > myStack.top()->val)
-                lastPopped = myStack.top(), myStack.pop();
+        // As long as the current value is greater, keep popping
+        while(ele > myStack.top()->val)
+            lastPopped = myStack.top(), myStack.pop();
             
-            // If nothing was popped, it means it was smaller, hence left child
-            if(!lastPopped)
-                myStack.top()->left = current;
-            else 
-                lastPopped->right = current;
-        }
+        // If nothing was popped, it means it was smaller, hence left child
+        if(!lastPopped)
+            myStack.top()->left = current;
+        else 
+            lastPopped->right = current;
         
         // Remember to push the new node . This is necessary to keep going
         myStack.push(current);
