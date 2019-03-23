c89cf0c49415ca02e34e0a718c6f69fca2ba2e42
Anonymous <48237538+Just-A-Visitor@users.noreply.github.com>
2019-03-23 11:03:50 +0530

    Update Concise and Optimized.cpp

--- LeetCode Contests/LeetCode Contest #126/#1003 Check if Word is Valid After Substitution/Concise and Optimized.cpp
+++ LeetCode Contests/LeetCode Contest #126/#1003 Check if Word is Valid After Substitution/Concise and Optimized.cpp
@@ -10,12 +10,12 @@ bool Solution :: isValid(string str)
     vector<char> specialStack;
     int n;
     
-	// Traverse the string and keep deleting 'abc' in O(1)
+    // Traverse the string and keep deleting 'abc' in O(1)
     for(auto ele : str)
     {
         n = specialStack.size();
         
-		// If the character is 'c', pop the matching 'b' and 'a' from the stack, if they exist 
+	// If the character is 'c', pop the matching 'b' and 'a' from the stack, if they exist 
         if(ele!= 'c' || n==0) specialStack.push_back(ele);
         else
             if(n>=2 && specialStack[n-1]=='b' && specialStack[n-2]=='a')
