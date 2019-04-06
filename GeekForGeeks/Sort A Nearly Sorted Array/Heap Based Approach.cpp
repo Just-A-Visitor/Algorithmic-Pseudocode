96298a110d579ff9d8308d1142805d0b2ccc4ca0
Anonymous <48237538+Just-A-Visitor@users.noreply.github.com>
2019-04-06 15:41:10 +0530

    Update Heap Based Approach.cpp

--- GeekForGeeks/Sort A Nearly Sorted Array/Heap Based Approach.cpp
+++ GeekForGeeks/Sort A Nearly Sorted Array/Heap Based Approach.cpp
@@ -1,3 +1,7 @@
+/*
+ * @author Just__a__Visitor
+ */
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -7,38 +11,47 @@ class Solution
     void sort_a_nearly_sorted_array(vector<int> &a, int k);
 };
 
+/* Sorts a nearly sorted array */
 void Solution :: sort_a_nearly_sorted_array(vector<int> &a, int k)
 {
-    // Size
+    // Extract the total numbers in the array
     int n = a.size();
-    
+
+    // The minimum and maximum displacement can be 0 and n-1
+    if(k>=n) k = n-1;
+
     // Create the min heap
     priority_queue<int, vector<int>, greater<int> > minHeap;
 
-    // Fill the first (k+1) elements in the min heap 
+    // Fill the first (k+1) elements in the min heap
     for(int i=0; i<(k+1); i++)
         minHeap.push(a[i]);
-    
-    int leftIndex = 0;
+
+    // Initialize the indexes
     int rightIndex = (k+1);
-    
-    // As long as the array is not overwritten, repeat
-    while(leftIndex<n)
+
+    /* At any time, leftIndex represents the index to be corrected
+     * rightIndex represents the first index (to the right) which
+     * is not present in the minHeap
+     */
+
+
+    // Keep correcting each of the indexes
+    for(int leftIndex=0; leftIndex<n; leftIndex++)
     {
         // Extract the minimum element
         int minElement = minHeap.top();
         minHeap.pop();
-        
+
         // Put the minimum in the correct place
         a[leftIndex] = minElement;
-        
-        // Push one element to keep the size of the  minHeap as (k+1 
-        if(rightIndex < n) minHeap.push(a[rightIndex]);
-        
-        // Increment both the indexes
-        
-        leftIndex++;
-        rightIndex++;
+
+        // Push one element to keep the size of the  minHeap as (k+1)
+        if(rightIndex < n) 
+        {
+            minHeap.push(a[rightIndex]);
+            rightIndex++;
+        }
     }
 }
 
@@ -50,26 +63,25 @@ void scanVector(vector<int> &a)
 
 void printVector(vector<int> &a)
 {
+    cout << "Printing the contents of the vector" << endl;
+  
     for(auto ele:a)
         cout << ele << " ";
-    
-    cout << endl;    
+
+    cout << endl << "The vector has been printed" << endl;
 }
 
-int main() 
+int main()
 {
-    int n;
-    cin >> n;
-    
-    int k;
-    cin >> k;
-    
-    vector<int> a(n);
+    int size,displacement;
+    cin >> size >> displacement;
+
+    vector<int> a(size);
     scanVector(a);
-    
+
     Solution myObject;
-    myObject.sort_a_nearly_sorted_array(a,k);
-    
+    myObject.sort_a_nearly_sorted_array(a,displacement);
+
     printVector(a);
 	return 0;
 }
