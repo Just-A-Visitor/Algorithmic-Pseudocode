9bd1ba9c893637c556970dd955f91546e825925e
Anonymous <48237538+Just-A-Visitor@users.noreply.github.com>
2019-08-13 14:40:30 +0530

    Create Naive LPS.cpp

---
+++ Codeforces Contests/CF #578 (Div 2)/F) Compress Strings/Naive LPS.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+vector<int> prefix_function(string str)
+{
+    int n = str.length();
+    vector<int> lps(n);
+    
+    for(int i=1; i<n; i++)
+    {
+        int j = lps[i-1];
+        while(j>0 and str[j]!=str[i])
+            j = lps[j-1];
+        
+        if(str[i]==str[j])
+            j++;
+        
+        lps[i] = j;
+    }
+    
+    return lps;
+}
+ 
+void merge_strings(vector<string>& a)
+{
+    int n = a.size();
+    string previousString = a[0];
+    
+    for(int i=1; i<n; i++)
+    {
+        string newString = a[i] + '#' + previousString;
+        vector<int> lps = prefix_function(newString);
+        
+        int overlapped_parts = lps.back();
+        for(int j=overlapped_parts; j<a[i].length(); j++)
+            previousString += a[i][j];
+    }
+    
+    cout << previousString << endl;
+}
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    
+    vector<string> a(n);
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+    
+    merge_strings(a);
+    
+	return 0;
+}
