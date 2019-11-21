712c67932babf05e1864d1196c9c42dcd466cd45
Anonymous <just1visitor@gmail.com>
2019-11-22 00:07:07 +0530

    Update Parallel BIT (Version 1).cpp

--- UVa Online Judge/Fenwick Tree/Interval Product (12532)/Parallel BIT (Version 1).cpp
+++ UVa Online Judge/Fenwick Tree/Interval Product (12532)/Parallel BIT (Version 1).cpp
@@ -128,9 +128,9 @@ string solve(int n)
 			
 			if(pos + neg != interval_len)
 				res += '0';
-			else  if(neg % 2 == 0)
+			else if(neg % 2 == 0)
 				res += '+';
-			else
+			else if(neg %2 != 0)
 				res += '-';
 
 		}
