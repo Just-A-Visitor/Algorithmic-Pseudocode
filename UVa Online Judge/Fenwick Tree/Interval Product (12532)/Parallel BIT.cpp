65d409f681d72b54bf14d28770143ec43de1c2a5
Anonymous <just1visitor@gmail.com>
2019-11-21 23:48:12 +0530

    Update Parallel BIT.cpp

--- UVa Online Judge/Fenwick Tree/Interval Product (12532)/Parallel BIT.cpp
+++ UVa Online Judge/Fenwick Tree/Interval Product (12532)/Parallel BIT.cpp
@@ -53,7 +53,8 @@ string solve(int n)
 
 	// One based indexing
 	vector<int> a(n + 1);
-	FenwickTree ft_pos(n), ft_neg(n);
+	FenwickTree ft_pos(n);
+	FenwickTree ft_neg(n)
 	
 	for(int i = 1; i < a.size(); i++)
 	{
